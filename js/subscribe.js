dbb6cf806d1505e330c85a8c22740460c4b6db53
JOiCh <ilieqreila@gmail.com>
2021-05-19 21:45:03 +0800

    第二版

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //subscribe動畫
     $('footer #submit').click(function () {
-        $(this).toggleClass('submitMove');//這行壞掉了
+        $(this).toggleClass('submitMove');
         $(this).html('<i class="far fa-check"></i>');
         // $(this).animate({width:'40px'},500)
 
